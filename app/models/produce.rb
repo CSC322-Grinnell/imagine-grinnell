7c915c3502e0daf7afbdf6dd1f4a3dc52f119dc8
Dhruv Phumbhra <phumbhra@grinnell.edu>
2019-02-27 20:59:19 +0000

    Fix case sensitivity uniqueness for name

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -1,6 +1,6 @@
 class Produce < ApplicationRecord
     belongs_to :garden
-    validates :name, uniqueness: true
+    validates :name, :uniqueness => {:case_sensitive => false}
     validates :name, :prediction_date, :readiness, presence: true
     validates :readiness, numericality: { greater_than_or_equal_to: 0, less_than: 3}
 end
