636c70f099d79c5b8bfd261a62748c4ab1502def
Liam Liden <l14m@msn.com>
2019-04-09 19:15:56 +0000

    change garden_produce to correct api specificiations

--- app/controllers/garden_produces_controller.rb
+++ app/controllers/garden_produces_controller.rb
@@ -2,63 +2,46 @@ class GardenProducesController < ApplicationController
   before_action :set_garden_produce, only: [:show, :edit, :update, :destroy]
 
   # GET /garden_produces
-  # GET /garden_produces.json
   def index
     @garden_produces = GardenProduce.all
+    render json: @garden_produces
   end
 
   # GET /garden_produces/1
-  # GET /garden_produces/1.json
   def show
-  end
-
-  # GET /garden_produces/new
-  def new
-    @garden_produce = GardenProduce.new
-  end
-
-  # GET /garden_produces/1/edit
-  def edit
+    render json: @garden_produce
   end
 
   # POST /garden_produces
-  # POST /garden_produces.json
+  skip_before_action :verify_authenticity_token
   def create
     @garden_produce = GardenProduce.new(garden_produce_params)
 
-    respond_to do |format|
-      if @garden_produce.save
-        format.html { redirect_to @garden_produce, notice: 'Garden produce was successfully created.' }
-        format.json { render :show, status: :created, location: @garden_produce }
-      else
-        format.html { render :new }
-        format.json { render json: @garden_produce.errors, status: :unprocessable_entity }
-      end
+    if @garden_produce.save
+      render json: @garden_produce, status: :created
+    else
+      render json: @garden_produce.errors, status: :unprocessable_entity
     end
+
   end
 
   # PATCH/PUT /garden_produces/1
-  # PATCH/PUT /garden_produces/1.json
-  def update
-    respond_to do |format|
-      if @garden_produce.update(garden_produce_params)
-        format.html { redirect_to @garden_produce, notice: 'Garden produce was successfully updated.' }
-        format.json { render :show, status: :ok, location: @garden_produce }
-      else
-        format.html { render :edit }
-        format.json { render json: @garden_produce.errors, status: :unprocessable_entity }
-      end
-    end
+  def update      
+     if @garden_produce.update(garden_produce_params)        
+        render json: @garden_produce, status: :ok
+     else        
+        render json: @garden_produce.errors, status: :unprocessable_entity      
+     end
   end
 
   # DELETE /garden_produces/1
-  # DELETE /garden_produces/1.json
   def destroy
     @garden_produce.destroy
-    respond_to do |format|
-      format.html { redirect_to garden_produces_url, notice: 'Garden produce was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    if @garden_produce.destroy
+      head :no_content, status: :ok
+    else
+      render json: @garden_produce.errors, status: :unprocessable_entity
+    end    
   end
 
   private
---
+++ spec/integration/garden_produce_spec.rb
@@ -0,0 +1,124 @@
+require 'swagger_helper'
+
+describe 'garden_produces API' do
+
+  path '/garden_produces' do
+    
+    post 'Creates a garden_produce' do
+      tags 'garden_produces'
+      consumes 'application/json', 'application/xml'
+      parameter name: :garden_produce, in: :body, schema: {
+        type: :object,
+        properties: {
+          garden_id: { type: :number },
+          produce_id: { type: :number },
+          available_at: { type: :string },
+          readiness: { type: :number },
+        },
+        required: [ 'garden_id', 'produce_id', 'available_at', 'readiness' ]
+      }
+
+      response '201', 'garden_produce created' do
+        let(:garden_produce) { { garden_id: '2', produce_id: '2', available_at: 'March 12', readiness: 0} }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:garden_produce) { { name: 'foo' } }
+        run_test!
+      end
+    end
+    
+    get 'Gets index of all garden_produce' do
+      tags 'garden_produces'
+      produces 'application/json', 'application/xml'
+
+      response '200', 'garden_produces found' do
+        schema type: :array,
+          items: {
+            properties: {
+              garden_id: { type: :number },
+              produce_id: { type: :number },
+              available_at: { type: :string },
+              readiness: { type: :number },
+            },
+          },
+          required: [ 'garden_id', 'produce_id', 'available_at', 'readiness' ]
+
+        let(:id) { Garden.create(garden_id: '2', produce_id: '2', available_at: 'March 12', readiness: 0).id }
+        let(:id) { Garden.create(garden_id: '1', produce_id: '2', available_at: 'March 13', readiness: 0).id }
+        run_test!
+      end
+    end
+  end
+
+  path '/garden_produces/{id}' do
+
+    get 'Retrieves a garden_produce' do
+      tags 'garden_produces'
+      produces 'application/json', 'application/xml'
+      parameter name: :id, :in => :path, :type => :string
+
+      response '200', 'name found' do
+        schema type: :object,
+          properties: {
+            garden_id: { type: :number },
+            produce_id: { type: :number },
+            available_at: { type: :string },
+            readiness: { type: :number },
+          },
+          required: [ 'garden_id', 'produce_id', 'available_at', 'readiness' ]
+
+        let(:id) { Garden.create(garden_id: '2', produce_id: '2', available_at: 'March 12', readiness: 0).id }
+        run_test!
+      end
+
+      response '404', 'garden_produce not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+    
+    patch 'Edits a garden_produce' do
+      tags 'garden_produces'
+      consumes 'application/json', 'application/xml'
+      parameter name: :id, :in => :path, :type => :string
+      parameter name: :garden_produce, in: :body, schema: {
+        type: :object,
+        properties: {
+          garden_id: { type: :number },
+          produce_id: { type: :number },
+          available_at: { type: :string },
+          readiness: { type: :number },
+        },
+          required: [ 'garden_id', 'produce_id', 'available_at', 'readiness' ]
+      }
+      
+      response '204', 'garden_produce changed' do
+        let(:id) { Garden.create(garden_id: '2', produce_id: '2', available_at: 'March 12', readiness: 0).id }
+        let(:garden_produce) { { garden_id: '2', produce_id: '8', available_at: 'March 12', readiness: 0 } }
+        run_test!
+      end
+      
+      response '404', 'garden_produce not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+    
+    delete 'Deletes a garden_produce' do
+      tags 'garden_produces'
+      parameter name: :id, :in => :path, :type => :string
+      
+      response '204', 'garden_produce deleted' do
+        let(:id) { Garden.create(garden_id: '2', produce_id: '2', available_at: 'March 12', readiness: 0).id }
+        run_test!
+      end
+      
+      response '404', 'garden_produce not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+  end
+end
