f6f457e54dbf9ad369737c5d3972573e96d8352e
Liam Liden <l14m@msn.com>
2019-04-09 19:18:27 +0000

    fix garden_produce tests

--- test/controllers/garden_produces_controller_test.rb
+++ test/controllers/garden_produces_controller_test.rb
@@ -10,17 +10,10 @@ class GardenProducesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_garden_produce_url
-    assert_response :success
-  end
-
   test "should create garden_produce" do
     assert_difference('GardenProduce.count') do
       post garden_produces_url, params: { garden_produce: { available_at: @garden_produce.available_at, garden_id: @garden_produce.garden_id, produce_id: @garden_produce.produce_id, readiness: @garden_produce.readiness } }
     end
-
-    assert_redirected_to garden_produce_url(GardenProduce.last)
   end
 
   test "should show garden_produce" do
@@ -35,14 +28,12 @@ class GardenProducesControllerTest < ActionDispatch::IntegrationTest
 
   test "should update garden_produce" do
     patch garden_produce_url(@garden_produce), params: { garden_produce: { available_at: @garden_produce.available_at, garden_id: @garden_produce.garden_id, produce_id: @garden_produce.produce_id, readiness: @garden_produce.readiness } }
-    assert_redirected_to garden_produce_url(@garden_produce)
+    assert_response :success
   end
 
   test "should destroy garden_produce" do
     assert_difference('GardenProduce.count', -1) do
       delete garden_produce_url(@garden_produce)
     end
-
-    assert_redirected_to garden_produces_url
   end
 end
