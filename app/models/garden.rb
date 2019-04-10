8ca3f86d95d9020054131dda59b7a80b5532081f
Dhruv Phumbhra <phumbhra@grinnell.edu>
2019-04-10 20:02:00 +0000

    Add comments to garden model

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,8 +1,10 @@
 class Garden < ApplicationRecord
     has_many :GardenProduces
-    validates :name, :address, :lat, :long, :contact_name, presence: true
-    validates :check_consistency
-    validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
+    validates :name, :address, :lat, :long, :contact_name, presence: true # all these fields are required
+    validates :check_consistency #a t least one of contact_number or email should be filled
+    # name must be only letters and some special chars
+    validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/ 
+    # email must be correct format
     validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
 
