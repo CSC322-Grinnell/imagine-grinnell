895e089f74692b7d7363fbde3d239675fd1099db
Dhruv Phumbhra <phumbhra@grinnell.edu>
2019-04-10 20:08:25 +0000

    Add validation for garden_produce model

--- app/models/garden_produce.rb
+++ app/models/garden_produce.rb
@@ -1,4 +1,5 @@
 class GardenProduce < ApplicationRecord
     belongs_to :Garden
     belongs_to :Produce
+    validates :produce_id, :garden_id, :available_at, :readiness, presence: true # all these fields are required
 end
