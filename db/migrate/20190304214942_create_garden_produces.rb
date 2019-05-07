368e937c3cbefd8942671757b2d5b4bee77555cd
Liam Liden <l14m@msn.com>
2019-05-07 18:28:30 +0000

    change gardenproduce id's into strings

--- db/migrate/20190304214942_create_garden_produces.rb
+++ db/migrate/20190304214942_create_garden_produces.rb
@@ -1,8 +1,8 @@
 class CreateGardenProduces < ActiveRecord::Migration[5.1]
   def change
     create_table :garden_produces do |t|
-      t.integer :garden_id
-      t.integer :produce_id
+      t.string :garden_id
+      t.string :produce_id
       t.string :available_at
       t.integer :readiness
       t.string :planted_at
