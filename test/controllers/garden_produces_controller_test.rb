0c1328fbc57961d8627a280b37add3642ca2f9ab
Liam Liden <l14m@msn.com>
2019-05-08 19:25:41 +0000

    add image to gardens

--- test/controllers/garden_produces_controller_test.rb
+++ test/controllers/garden_produces_controller_test.rb
@@ -24,11 +24,6 @@ class GardenProducesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_garden_produce_url(@garden_produce)
-    assert_response :success
-  end
-
   test "should update garden_produce" do
     patch garden_produce_url(@garden_produce), params: { garden_produce: { 
       available_at: @garden_produce.available_at, garden_id: @garden_produce.garden_id, produce_id: @garden_produce.produce_id, 
