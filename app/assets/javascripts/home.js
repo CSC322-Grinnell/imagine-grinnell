8a39e5ff6435ef2f0d2c5e68dd777feaca371111
Evan Holt <holtevan@grinnell.edu>
2019-10-07 11:26:10 -0500

    Reformat inline HTML into template strings
    Make inline HTML in home.js readable, formatted as HTML should be

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -148,58 +148,71 @@ function initMap() {
   var infowindow = new google.maps.InfoWindow();
   
   getJSON("./gardens", function populate_map(data){
-   var gardens = [...Array(data.length)].map(e => Array(4));
-	 for(var i = 0; i < data.length; i++){
-	   gardens[i][0]=data[i].name;
-	   gardens[i][1]=data[i].lat;
-	   gardens[i][2]=data[i].long;
-	   gardens[i][3]="<div class=\"card z-depth-0\" style=\"width: 100%;\"><div class=\"card-image\"><img src=\""+
-	   data[i].image + "\"><span class=\"card-title\">" +
-	   data[i].name + "</span></div><div class=\"card-content\"><p>" + 
-	   data[i].contact_name + "</p><p>" + 
-	   data[i].contact_number + "</p><p>" + 
-	   data[i].address + "</p><ul id=garden_id" + 
-	   data[i].id + " class=\"collection\"></ul></div></div>";
-	   var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(gardens[i][1], gardens[i][2]),
-      title: gardens[i][0],
-      map: map
-	   });
-	   google.maps.event.addListener(marker, 'click', (function(marker, i) {
-      return function() {
-      infowindow.setContent(gardens[i][3]);
-      infowindow.open(map, marker);
-      populate_produces();
-      }
-     })(marker, i));
-	 }
+    var gardens = [...Array(data.length)].map(e => Array(4));
+    for(var i = 0; i < data.length; i++){
+      gardens[i][0] = data[i].name;
+      gardens[i][1] = data[i].lat;
+      gardens[i][2] = data[i].long;
+      gardens[i][3] = `
+        <div class="card z-depth-0" style="width: 100%;">
+          <div class="card-image">
+            <img src="${data[i].image}">
+            <span class="card-title">
+              ${data[i].name}
+            </span>
+          </div>
+          <div class="card-content">
+            <p>${data[i].contact_name}</p>
+            <p> ${data[i].contact_number}</p>
+            <p>${data[i].address}</p>
+            <ul id=garden_id ${data[i].id} class="collection"></ul>
+          </div>
+        </div>`;
+      
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(gardens[i][1], gardens[i][2]),
+        title: gardens[i][0],
+        map: map
+      });
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(gardens[i][3]);
+          infowindow.open(map, marker);
+          populate_produces();
+        }
+      })(marker, i));
+    }
   }, function(status) {
-	  alert('Something went wrong.');
+    alert('Something went wrong.');
   });
 }
 
 function populate_produces(){
-	 getJSON("./garden_produces", function(data) {
-	  for(var i = 0; i <= data.length - 1; i++){
-	   try{
-     document.getElementById('garden_id'+data[i].garden_id).innerHTML += 
-     ("<li class=\"collection-item light-green lighten-" + (3 - data[i].readiness) + "\"><span class=\"produce_id" + data[i].produce_id + "\"></span>, " + data[i].available_at + "</li>")
-	   } catch {
-	   }
-	  }
-	  getJSON("./produces", function populate_table_helper_produces(data){
-	   for(var i = 0; i <= data.length - 1; i++){
-	    var elements = document.getElementsByClassName('produce_id'+i);
-	    for(var j = 0; j < elements.length; j++){
-       elements[j].innerHTML += data[i].name;
-	    }
-	   }
+  getJSON("./garden_produces", function(data) {
+    for(var i = 0; i <= data.length - 1; i++){
+      try{
+        document.getElementById("garden_id" + data[i].garden_id).innerHTML += (`
+          <li class="collection-item light-green lighten-${(3 - data[i].readiness)}">
+            <span class="produce_id${data[i].produce_id}"></span>,
+            ${data[i].available_at}
+          </li>`
+        )
+      } catch {
+      }
+    }
+    getJSON("./produces", function populate_table_helper_produces(data){
+      for(var i = 0; i <= data.length - 1; i++){
+        var elements = document.getElementsByClassName('produce_id'+i);
+        for(var j = 0; j < elements.length; j++){
+          elements[j].innerHTML += data[i].name;
+        }
+      }
     }, function(status) {
-	    alert('Something went wrong.');
+      alert('Something went wrong.');
     });
-   }, function(status) {
-	   alert('Something went wrong.');
-   });
-  }
+  }, function(status) {
+    alert('Something went wrong.');
+  });
+}
   
   
