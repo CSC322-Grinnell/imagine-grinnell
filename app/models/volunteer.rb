42f42eb928b871c75f431b996f5514388310b83b
Evan Holt <holtevan@grinnell.edu>
2019-10-30 22:20:07 -0500

    Add presence validation to volunteer model
    Prevents a volunteer from being saved to the database if required
    fields are missing or the email address does not follow proper format

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,2 +1,5 @@
 class Volunteer < ApplicationRecord
+  validates :email, :password, :salt, presence: true # all these fields are required
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ # email must have correct format
+  before_save { email.downcase! }
 end
