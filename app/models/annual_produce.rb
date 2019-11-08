0bed13196d15b815ac0ea486cb7c7dd3038c420b
kotaniko <kotaniko@grinnell.edu>
2019-11-08 11:57:12 -0600

    Fix error occurred when bin/initialize.sh is executed
    REGX in model files for annual and perennial produce were broken, so fixed them. Also, added volunteer seeding.

--- app/models/annual_produce.rb
+++ app/models/annual_produce.rb
@@ -1,6 +1,6 @@
 class AnnualProduce < Produce
     has_many :Crops
-    validates :name, presence: true, format: { with: /\a[a-za-z]*\z/ } #check if the name has only letters
+    validates :name, presence: true, format: { with: /\A[a-zA-Z]*\Z/ } #check if the name has only letters
     validates :name, :uniqueness => {:case_sensitive => false} #only one produce of a particular name should exist
-    validates :duration, presence: true, format: { with: /\a[0-9]*\z/ } #duration should only be numbers
+    validates :duration, presence: true, format: { with: /\A[0-9]*\Z/ } #duration should only be numbers
 end
