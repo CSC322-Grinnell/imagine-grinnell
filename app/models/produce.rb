3b90f260e4c630c300988c660afbf837898dc304
kotaniko <kotaniko@grinnell.edu>
2019-11-07 21:27:23 -0600

    Add annual and perennial produce tables for STI
    A type field is added to the produce table for single table inheritance. Then, two tables, AnnualProduce and PerennialProduce are created inheriting from Produce.

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -1,10 +1,2 @@
 class Produce < ApplicationRecord
-    has_many :GardenProduces
-    validates :name, presence: true, format: { with: /\A[a-zA-Z]*\z/ } #check if the name has only letters
-    validates :name, :uniqueness => {:case_sensitive => false} #only one produce of a particular name should exist
-    validates :duration, presence: true, format: { with: /\A[0-9]*\z/ } #duration should only be numbers
 end
-
-# Hello
-# Please log out of your accounts when done
-# As punishment I deleted random lines of your code 
