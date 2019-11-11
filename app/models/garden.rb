f676e87eac53d129199f0da1abe671ca82656275
YourFin <d.junkpobox+git@gmail.com>
2019-11-11 07:34:05 -0800

    Fix syntax error introduced in rebase

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,23 +1,22 @@
 class Garden < ApplicationRecord
-    has_many :GardenProduces
-    validates :name, :address, :lat, :long, :contact_name_1, presence: true # all these fields are required
-    # validates :check_consistency #a t least one of contact_number or email should be filled
-    # name must be only letters and some special chars
-    validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
-    # email must be correct format
-    validates_format_of :email_1,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  has_many :GardenProduces
+  validates :name, :address, :lat, :long, :contact_name_1, presence: true # all these fields are required
+  # validates :check_consistency #a t least one of contact_number or email should be filled
+  # name must be only letters and some special chars
+  validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
+  # email must be correct format
+  validates_format_of :email_1,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
 
-    def check_consistency
-      if contact_number_1.blank? and email_1.blank?
-       #one at least must be filled in, add a custom error message
-       return false
-      elsif !contact_number_1.blank? and !email_1.blank?
-       #both can't be filled in, add custom error message
-       return false
-      else
-       return true
-      end
+  def check_consistency
+    if contact_number_1.blank? and email_1.blank?
+      #one at least must be filled in, add a custom error message
+      return false
+    elsif !contact_number_1.blank? and !email_1.blank?
+      #both can't be filled in, add custom error message
+      return false
+    else
+      return true
     end
   end
 end
