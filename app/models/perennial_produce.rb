b3f345ea8d22402be24fc5a42a8172e16c2ee316
kotaniko <kotaniko@grinnell.edu>
2019-11-20 21:45:41 -0600

    Add test cases for Annual and Perennial Produce
    Produces controller has to be fixed so that it can handle annual and perennial STI.

--- app/models/perennial_produce.rb
+++ app/models/perennial_produce.rb
@@ -2,5 +2,5 @@ class PerennialProduce < Produce
   has_many :Crops
   validates :name, presence: true, format: { with: /\A[a-zA-Z]*\Z/ } # check if the name has only letters
   validates :name, :uniqueness => { :case_sensitive => false } # only one produce of a particular name should exist
-  validates :duration, presence: true, format: { with: /\A[0-9]*\Z/ } # duration should only be numbers
+  validates :duration, presence: true, format: { with: /\A[0-9]*\Z/, message: "duration should only be numbers" }
 end
--- app/models/produce.rb
+++ app/models/produce.rb
@@ -1,7 +1,6 @@
 class Produce < ApplicationRecord
-  self.primary_key = 'id'
   has_many :Crops
   validates :name, presence: true, format: { with: /\A[a-zA-Z]*\Z/ } # check if the name has only letters
   validates :name, :uniqueness => { :case_sensitive => false } # only one produce of a particular name should exist
-  validates :duration, presence: true, format: { with: /\A[0-9]*\Z/ } # duration should only be numbers
+  validates :type, presence: true, format: { with: /\A(PerennialProduce)|(AnnualProduce)\Z/, message: "Only PerennialProduce or AnnualProduce is allowed" } # check if the name has only letters
 end
