9399d586ba5aa14dcf7e7b3428f659c8e3b12e54
Evan Holt <holtevan@grinnell.edu>
2019-11-22 10:11:54 -0600

    Add ripeness column

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,10 +11,30 @@ class StaticPagesController < ApplicationController
     Crop.all.each do |crop|
       garden_name = Garden.find(crop.garden_id).name
 
-      crop_produce_name = Produce.find(crop.produce).name
-      crop_item = [crop_produce_name]
-      crop_item << crop.description ? crop.description : '-'
+      crop_produce = Produce.find(crop.produce)
+      crop_item = [crop_produce.name] # Add the name
+      crop_item << if crop.description # Add the description, if there is one
+        crop.description
+      else
+        '-'
+      end
+      
+      # Calculate days to ripeness
+      # WAIT UNTIL DATES CHANGED IN DB
+      # Plan for annual produce:
+      #  Get date planted in date object form
+      #  Add produce.duration (an int) to that date object, yields date when crop will be ripe
+      #  Get current date in date object form
+      #  Determine the difference in days between current date and date when ripe
+      #  If date when ripe is in the past, then the crop is ripe now
+      #  If the crop is not ripe now, list the date when ripe
+      # Plan for perennial produce:
+      #  Get current date in date object form
+      #  If the current date is within the date range given in the produce, then the crop is now ripe
+      #  If the crop is not ripe now, list the ripeness date range
+      crop_item << "[ Ripeness Here ]" # placeholder
 
+      # Assemble the array of all crops
       if @all_crops[garden_name]
         @all_crops[garden_name] << crop_item
       else
