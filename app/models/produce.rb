18a1f237f4085b34f1c25de7c199fb0f8538e034
snod4 <csnodgrass233@gmail.com>
2019-11-25 11:10:01 -0600

    Resolve id error in database

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -1,4 +1,5 @@
 class Produce < ApplicationRecord
+  self.primary_key = 'id'
   has_many :Crops
   validates :name, presence: true, format: { with: /\A[a-zA-Z]*\Z/ } # check if the name has only letters
   validates :name, :uniqueness => { :case_sensitive => false } # only one produce of a particular name should exist
