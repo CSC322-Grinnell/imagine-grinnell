b68dae1a67ef962b3da2d98e3bbd4802b48e475a
kotaniko <kotaniko@grinnell.edu>
2019-11-26 21:10:17 -0600

    Fix code style to suppress ruby lint errors

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -2,5 +2,5 @@ class Produce < ApplicationRecord
   has_many :Crops
   validates :name, presence: true, format: { with: /\A[a-zA-Z]*\Z/ } # check if the name has only letters
   validates :name, uniqueness: { case_sensitive: false } # only one produce of a particular name should exist
-  validates :type, presence: true, format: { with: /\A(PerennialProduce)|(AnnualProduce)\Z/, message: "Only PerennialProduce or AnnualProduce is allowed" } # check if the name has only letters
+  validates :type, presence: true, format: { with: /\A(PerennialProduce)|(AnnualProduce)\Z/, message: 'Only PerennialProduce or AnnualProduce is allowed' } # check if the name has only letters
 end
