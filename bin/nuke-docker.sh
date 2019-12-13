fabe9b3e4c4376df4a9150cea39b43932451ae03
YourFin <d.junkpobox+git@gmail.com>
2019-12-12 19:45:05 -0800

    Add help text for bin/nuke-docker.sh

--- bin/nuke-docker.sh
+++ bin/nuke-docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-NO_CLOBBER=false
-FORCE_CLOBBER=false
+BYPASS=false
+HELP=false
+
+SCRIPT_NAME="nuke-docker.sh"
 
 # Parse command line args
 POSITIONAL=()
@@ -10,15 +12,14 @@ do
     key="$1"
 
     case $key in
-        -n|--no-clobber)
-            NO_CLOBBER=true
+        -h|--help)
+            # Clobber everything
+            HELP=true
             shift # past argument
             ;;
-        -f|--force)
-            # Clobber everything
-            FORCE_CLOBBER=true
-            shift # past argument
-            shift # past value
+        -y|--yes)
+            BYPASS=true
+            shift
             ;;
         *)    # unknown option
             POSITIONAL+=("$1") # save it in an array for later
@@ -32,7 +33,37 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 set -euo pipefail
 IFS=$'\n\t'
 
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+if $HELP ; then
+    echo "$SCRIPT_NAME removes ALL docker information on disk."
+    echo "After running this command, you will lose ALL your database"
+    echo "information, ALL images, and have to re-download and build EVERYTHING."
+    echo "You should really only run this script when your computer is out of"
+    echo "disk space, or if you've spent two hours trying to solve an"
+    echo "un-reproduceable bug with docker on your machine."
+    echo
+    echo "Usage:"
+    echo "$SCRIPT_NAME [no parameters]"
+    echo "  -y or --yes          Ignore the warning"
+    echo "  -h or --help         Print this message"
+    exit 0
+fi
+
+
+if ! $BYPASS ; then
+    echo "WARNING: This will remove ALL docker data, short of the docker/docker-compose"
+    echo "commands themselves. After running this command, you will lose ALL your database"
+    echo "information, ALL images, and have to re-download and build EVERYTHING."
+    echo
+    echo "You should really only run this script when your computer is out of disk space."
+    read -p "Are you sure that you want to continue? [y/n] " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        exit 1
+    fi
+fi
+
+exit 0
 
 echo
 echo "Nuking containers..."
