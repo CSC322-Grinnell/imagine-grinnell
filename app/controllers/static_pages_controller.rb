cb222996faad708bd3ce3556549e60990525a79e
Evan Holt <holtevan@grinnell.edu>
2019-12-12 22:06:09 -0600

    Fix linting issues

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,23 +15,21 @@ class StaticPagesController < ApplicationController
       crop_item = {
         :name => crop_produce.name,
         :desc => '-',
-        :ripe => '-'
+        :ripe => '-',
       }
-      if crop.description # Add the description, if there is one
-        crop_item[:desc] = crop.description
-      end
-      
+      crop_item[:desc] = crop.description if crop.description # Add the description, if there is one
+
       # Determine ripeness
       today = Date.today
-      if crop_produce.type == "AnnualProduce"
-        planted_date = Date::strptime(crop.planted_at, '%m/%d/%Y')
+      if crop_produce.type == 'AnnualProduce'
+        planted_date = Date.strptime(crop.planted_at, '%m/%d/%Y')
         ripe_date = planted_date + crop_produce.duration
-        is_ripe = today <=> ripe_date >= 0 # means: is today on or after ripe_date
+        is_ripe = (today <=> ripe_date) >= 0 # means: is today on or after ripe_date
         crop_item[:ripe] = if is_ripe
-          'Ripe now'
-        else
-          ripe_date.strftime('Ripe on %-m/%-d/%Y')
-        end
+                             'Ripe now'
+                           else
+                             ripe_date.strftime('Ripe on %-m/%-d/%Y')
+                           end
       else
         start_date_temp = crop_produce.start_date
         end_date_temp = crop_produce.end_date
@@ -40,7 +38,7 @@ class StaticPagesController < ApplicationController
         # cause the start date to come after the end date
         start_date = Date.new(today.year, start_date_temp.month, start_date_temp.day)
         end_date = Date.new(today.year, end_date_temp.month, end_date_temp.day)
-        if start_date <=> end_date > 0 # means: does start_date come after end_date
+        if (start_date <=> end_date).positive? # means: does start_date come after end_date
           # If the two lines above broke that new year thing, this will fix it
           end_date >> 12 # subtract a year
         end
@@ -63,10 +61,10 @@ class StaticPagesController < ApplicationController
 
         is_ripe = is_ripe_first_range || is_ripe_second_range
         crop_item[:ripe] = if is_ripe
-          'Ripe now'
-        else
-          `Ripe between #{start_date.strftime('%-m/%-d')} and #{end_date.strftime('%-m/%-d')}`
-        end
+                             'Ripe now'
+                           else
+                             `Ripe between #{start_date.strftime('%-m/%-d')} and #{end_date.strftime('%-m/%-d')}`
+                           end
       end
 
       # Assemble the array of all crops
